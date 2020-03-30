993b1684b6e57bb450e5a49cad3c45b7087f0dec
Samuel Magalhães <samuelmagalhaes@id.uff.br>
2020-03-30 19:52:45 -0300

    Back-end do app

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,7 +5,6 @@ const incidentController = require('./controllers/incidentController');
 const profileController = require('./controllers/profileController');
 const sessionController = require('./controllers/sessionController');
 
-
 const routes = express.Router();
 
 routes.post('/sessions', sessionController.create);
